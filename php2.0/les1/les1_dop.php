76d433123c37fa886b473eec8ca17523a05d0fd9
krafik <alexeym4l@mail.ru>
2019-03-28 21:23:13 +0300

    урок 1. 1 задание и 1 доп задание.

---
+++ php2.0/les1/les1_dop.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexe
+ * Date: 28.03.2019
+ * Time: 19:40
+ */
+/*
+$a=10;
+
+$p = "a"; //присваиваем пеменной имя другой переменно
+$b = "p";
+$c = "b";
+
+
+echo $$p.'<br>';
+echo $$b.'<br>';
+echo $$c.'<br>';
+*/
+$a = "hello"; //появилась переменная hello!
+$$a = "world"; // $hello = world!
+$$$a = "this ";
+$$$$a = "is";
+$$$$$a = "holy hierarchy";
+
+
+
+echo "$a $hello $world $this_ $is";
+
+
+
+
+
+/*
+давайте рассмотрим доп урок. Там переменные по ссылке надо присваивать?
+    т.е $a = $b;
+т.е $a = b; $&b= $a*/
